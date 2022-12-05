9f37916e56bcd518c5b024d00618bda4075546b6
Dungcnttimd <dungcnttimd@gmail.com>
2022-12-05 18:58:38 +0700

    .

--- GameTestGuide/BlockGame/Assets/Script/PlayerStatus.cs
+++ GameTestGuide/BlockGame/Assets/Script/PlayerStatus.cs
@@ -19,6 +19,8 @@ public class PlayerStatus : MonoBehaviour
     public List<GameObject> gameobject_BackgroundGem;
 
     public bool b_GemSelected = false;
+
+    public bool b_GemSelecting = false;
     // Animator animator;
 /*
     If player click <Pause button>  -> b_Play_Pause = true
@@ -26,10 +28,7 @@ public class PlayerStatus : MonoBehaviour
     If this game has finnish u must set -> b_Reset = true  -> player wil be click <Reset button> to continue
 
 
-
-
-
-
+    
 */
     void Awake(){
         b_GemSelected = false;
@@ -39,15 +38,55 @@ public class PlayerStatus : MonoBehaviour
             gameobject_Gem[i].GetComponent<GemIndex>().i_GemIndex = i;
         }
     }
+    Animator[] gemZoom = new Animator[64];
+    Animator[] gemBackgourndGradient = new Animator[64];
     // Start is called before the first frame update
     void Start(){
-        
+        for(int i = 0; i < i_SizeTotal; i += 1){
+            gemZoom[i] = gameobject_Gem[i].GetComponent<Animator>();
+            gemBackgourndGradient[i] = gameobject_BackgroundGem[i].GetComponent<Animator>();
+        }
     }
 
     public void Set_ID_All(int id){
         for(int i = 0; i < i_SizeTotal; i += 1){
             gameobject_Gem[i].GetComponent<GemIndex>().i_index = id;
         }
+    }
+    private void process(){
+        Debug.Log("Process");
+        if(b_GemSelected == true){
+            Debug.Log("Animation");
+            int x = iIndexSelected % i_SizeW + 1;
+            int y = iIndexSelected / i_SizeH + 1;
+            gemZoom[iIndexSelected].SetBool("Selected", true);                  //1
+            Debug.Log(x + " - " + y);
+
+            if(y == 1){
+                gemBackgourndGradient[iIndexSelected+i_SizeW].SetBool("Active", true);
+            }
+            if(y == i_SizeH){
+                gemBackgourndGradient[iIndexSelected-i_SizeW].SetBool("Active", true);
+            }
+            if(y != 1 && y != i_SizeH){
+                gemBackgourndGradient[iIndexSelected+i_SizeW].SetBool("Active", true);
+                gemBackgourndGradient[iIndexSelected-i_SizeW].SetBool("Active", true);
+            }
+
+            if(x == 1){
+                gemBackgourndGradient[iIndexSelected+1].SetBool("Active", true);
+            }
+            if(x == i_SizeW){
+                gemBackgourndGradient[iIndexSelected-1].SetBool("Active", true);
+            }
+            if(x != 1 && x != i_SizeW){
+                gemBackgourndGradient[iIndexSelected+1].SetBool("Active", true);
+                gemBackgourndGradient[iIndexSelected-1].SetBool("Active", true);
+            }
+            
+
+        }
+        // gemZoom[0].SetBool("Active", true);
     }
     // Update is called once per frame
     void Update(){
@@ -59,9 +98,12 @@ public class PlayerStatus : MonoBehaviour
                 }
             }
             iIndexSelected = val;
+            // // Debug.Log(val);
+            // Debug.Log((val%8 + 1) + " - " +  (val/8 + 1) + " is index");
+            // Debug.LogWarning(iIndexSelected);
+
+            process();
             b_GemSelected = false;
-            // Debug.Log(val);
-            Debug.Log((val%8 + 1) + " - " +  (val/8 + 1) + " is inde");
         }
         // if((int)Time.time % 5 == 0){                                                                //done get selected index   
         //     for(int i = 0; i < i_SizeTotal; i += 1){                                                //gameobject_Gem[i].GetComponent<GemIndex>().i_index
